8adc0d84760744bd9bfe1ea87a178d7e4371dbeb
bbharatchandra <110092341+bbharatchandra@users.noreply.github.com>
2022-07-27 15:16:07 +0530

    Update streamlit_app.py
    Added quotes within the brackets to correct the syntax.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 Streamlit.Header("Breakfast Menu")
-Streamlit.text(Omega 3 & Bllueberry Oatmeal)
-Streamlit.text(Kale, Spinach & Rocket Smoothie)
-Streamlit.text(Hard-Boiled Free-Range Egg)
+Streamlit.text("Omega 3 & Blueberry Oatmeal")
+Streamlit.text("Kale, Spinach & Rocket Smoothie")
+Streamlit.text("Hard-Boiled Free-Range Egg")
